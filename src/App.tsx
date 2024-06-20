89a831e996bb9e104852e87d4af6ff4117a8d623
Arthur Lin <arslin@ucsc.edu>
2024-06-20 12:21:49 -0700

    error fix for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Header";
 import Hero from "./components/Hero";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -11,14 +12,14 @@ function App() {
       
        {/* Navbar component will still render */}
       {/* Commenting out the Routes for now */}
-      {/* <Routes>
+      {/* { <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/skills" element={<Skills />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/experience" element={<Experience />} />
-            </Routes> */}
+            </Routes> } */}
     </Router>
   );
 }
